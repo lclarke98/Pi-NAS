836a9f3c61f7cd02b4fa8a68db46c2cc173d25bb
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2019-09-26 16:45:52 +0100

    added more script items

--- shell-scripts/setup.sh
+++ shell-scripts/setup.sh
@@ -1,8 +1,14 @@
+# Initial Setup
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install ntfs-3g
+sudo apt-get install ntfs-3g -y
 sudo apt install apache2 -y
 sudo apt-get install samba samba-common-bin -y
 
+
+# Gives user file permissions
+sudo chmod a+rw /var/www
+
+# Adding api to webserver folder
 cd /var/www
 git clone https://github.com/lclarke98/Pi-NAS
