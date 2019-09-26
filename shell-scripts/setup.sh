3ca9469cc9a36dbc7726bb82fabd58ce5565981d
Leo <leo@Leos-Mac-mini.connect>
2019-09-26 14:40:29 +0100

    added new command

--- shell-scripts/setup.sh
+++ shell-scripts/setup.sh
@@ -3,3 +3,6 @@ sudo apt-get upgrade -y
 sudo apt-get install ntfs-3g
 sudo apt install apache2 -y
 sudo apt-get install samba samba-common-bin -y
+
+cd /var/www
+git clone https://github.com/lclarke98/Pi-NAS
